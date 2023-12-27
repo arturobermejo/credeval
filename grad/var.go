549610a387dcecccf3477fbe20c0c3e32042a32e
Arturo Bermejo <arturobermejo.gs@gmail.com>
2023-12-27 11:20:26 -0500

    init grad package and add var type

---
+++ grad/var.go
@@ -0,0 +1,33 @@
+package grad
+
+import "fmt"
+
+type Var struct {
+	value    float64
+	op       string
+	backward func()
+	grad     float64
+	children []*Var
+}
+
+func NewVar(v float64) *Var {
+	return &Var{
+		value:    v,
+		op:       "none",
+		backward: func() {},
+		grad:     1.0,
+		children: make([]*Var, 0),
+	}
+}
+
+func (v *Var) String() string {
+	return fmt.Sprintf("var(%f, op=<%s>)", v.value, v.op)
+}
+
+func (v *Var) Value() float64 {
+	return v.value
+}
+
+func (v *Var) Grad() float64 {
+	return v.grad
+}
